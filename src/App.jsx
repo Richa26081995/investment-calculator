3eb9b005304e0c7216ac18e77f6b8ddb24f56b00
Richa Mahaeshwari <richamaheshwari2631@gmail.com>
2024-05-19 20:07:52 +0530

    header component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./Components/Header";
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header></Header>
+    </>
+  );
 }
 
-export default App
+export default App;
